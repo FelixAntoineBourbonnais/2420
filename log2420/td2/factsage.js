10e790909708023f545e1e4251f8672b802b978a
Noboru Yoshida <noboruyoshida1997@gmail.com>
2019-02-18 15:11:53 -0500

    NY: D added + button functionnality

--- log2420/td2/factsage.js
+++ log2420/td2/factsage.js
@@ -10,3 +10,14 @@ function clearAll() {
         inputData[i].value = '';
     }
 }
+
+function addElement() {
+    var massToAdd = document.getElementById("massElement").innerHTML + "<br>";
+    var speciesToAdd = document.getElementById("speciesElement").innerHTML + "<br>";
+    
+    document.getElementById("addedMass").style.display = "inline";
+    document.getElementById("addedSpecies").style.display = "inline";
+    
+    document.getElementById("addedMass").innerHTML += massToAdd;
+    document.getElementById("addedSpecies").innerHTML += speciesToAdd;
+}
