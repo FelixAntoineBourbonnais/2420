e9c7e61b4be6e4019d9db34c50fc0f690016b73a
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-02-18 15:19:20 -0500

    update

--- log2420/td2/factsage.js
+++ log2420/td2/factsage.js
@@ -18,7 +18,7 @@ function calculate() {
 
     for (i = 0; i < inputData.length; i++) {
         if (inputData[i].value.length !== 0) {
-            if (isNumeric(inputData[i].value) || (i+1) === inputData.length) {
+            if (isNumeric(inputData[i].value) || (i + 1) === inputData.length) {
                 formule = formule + inputData[i].value + ' ';
             } else {
                 formule = formule + inputData[i].value + ' + ';
@@ -32,7 +32,19 @@ function calculate() {
 }
 
 function postFormula(formule) {
-
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("table").innerHTML = this.responseText;
+        }
+        else {
+            alert('Error: ' + xhttp.status);
+        }
+    };
+    xhttp.open("POST", "http://localhost:8080/", true);
+    xhttp.withCredentials = true;
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send({ 'request': "authentication token" });
 }
 
 function isNumeric(n) {
