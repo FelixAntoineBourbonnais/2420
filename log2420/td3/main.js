52858e2f2258553afb4b14860aafbbd2e6068e5e
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-15 23:16:48 -0400

    sdsad

---
+++ log2420/td3/main.js
@@ -0,0 +1,16 @@
+function toggleJoin() {
+    togglePlusMinus();
+    joinChannel();
+}
+
+function togglePlusMinus() {
+    $(document).ready(function(){
+        alert("sfdfgfdgfdg");
+        $("#plus").toggleClass("");
+    });
+
+}
+
+function joinChannel() {
+
+}
