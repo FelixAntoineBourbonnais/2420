92595fefe878b9b2161ee4aa9ab58c8995073337
Felix <felix-atnoine.bourbonnais@polymtl.ca>
2019-03-29 20:08:31 -0400

    ssssaas

--- log2420/td3/connectionHandler.js
+++ log2420/td3/connectionHandler.js
@@ -1,24 +1,5 @@
 
-<<<<<<< HEAD
 socket.onmessage = function (event) {
-=======
-var socket = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice?username=" + "Simon");
-
-//var socket = new WebSocket("ws://localhost:3000");
-
-socket.onopen = function() {
-  sendText(message);
-}
-
-function sendText(message) {
-  console.log("readystate = " + socket.readyState);
-  if(socket.readyState === 1) {
-    socket.send(JSON.stringify(message));
-  }
-}
-
-socket.onmessage = function(event) {
->>>>>>> 9b9e6ef2de85ca45352d3746e6ff46622f88156e
     let msg = JSON.parse(event.data);
     console.log(msg);
     switch (msg.eventType) {
